c08998b548393de507ce0361bfdb1ce8c2ada1f1
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2024-05-27 17:44:08 +0700

    Initial firebase

---
+++ config/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
